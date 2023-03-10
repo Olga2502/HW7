1cd956482138dfaf17a1ce02203db8bc0aa25705
Olga <101519229+Olga2502@users.noreply.github.com>
2023-03-10 18:23:27 +0300

    Задача 52 нахождение среденеарифметического строк двумерного массива

---
+++ EX52/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SrArf(int[,] inArray)
+{
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            sum += inArray[i, j];
+        }
+        double sreArf = Math.Round(sum/inArray.GetLength(0),1);
+        Console.Write($"{sreArf}   ");
+    }
+}
+
+
+Console.Write("Введите количество строк: ");
+int st = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(st, col, -10, 10);
+
+Console.WriteLine("Массив чисел: ");
+PrintArray(array);
+Console.WriteLine();
+SrArf(array);
+
+
